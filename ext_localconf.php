3981deffef46b226b8488050e68c39558e49349d
DDEV User <nobody@example.com>
2026-03-11 17:16:45 -0600

    Css and Js moved to typoscript

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,6 @@
 <?php
 defined('TYPO3') || die();
 
-use TYPO3\CMS\Core\Page\AssetCollector;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
-
 (function () {
 
     $GLOBALS['TYPO3_CONF_VARS']['FE']['typolinkBuilder']['photoswipe'] =
@@ -12,18 +8,4 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['linkHandler']['photoswipe'] =
         \Tei\PhotoSwipe\LinkHandler\PhotoSwipeLinkHandling::class;
 
-    /** @var AssetCollector $assetController */
-    $assetController = GeneralUtility::makeInstance(AssetCollector::class);
-    $assetController->addJavaScript(
-        'photoswipe',
-        'EXT:photoswipe/Resources/Public/JavaScript/photoswipe-init.js',
-        [
-            'type' => 'module'
-        ]);
-    $assetController->addStyleSheet(
-        'photoswipe',
-        'EXT:photoswipe/Resources/Public/Css/photoswipe.css');
-    $assetController->addStyleSheet(
-        'photoswipe-dynamic-caption',
-        'EXT:photoswipe/Resources/Public/Css/photoswipe-dynamic-caption-plugin.css');
 })();
